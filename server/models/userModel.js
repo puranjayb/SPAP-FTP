ca1d21964e8b2182ce6ad9938ce1c752386a1db6
puranjayb <pa4257@srmist.edu.in>
2024-04-07 14:54:06 +0530

    Server init and setup

---
+++ server/models/userModel.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const connection = require('../config/dbConfig');
+const bcrypt = require('bcrypt');
+
+const User = connection.define('User', {
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+});
+
+module.exports = User;
