cf7aaf64b8a2822006e99d7e55736abc5fc36ac6
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-22 12:47:09 +0530

    Minor fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./Layout"
 import Dashboard from "./page/Dashboard"
+import Login from "./page/Login"
+import TotalExpenses from "./page/TotalExpenses"
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/total-expense" element={<TotalExpenses />} />
         </Route>
       </Routes>
     </>
